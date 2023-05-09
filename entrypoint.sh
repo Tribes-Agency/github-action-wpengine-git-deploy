ac3723a9a47f40ab9a57e2d7e27d5b18b9b73a5b
Steven J. Wright <steven@tribes.work>
2023-05-09 11:26:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,8 +28,8 @@ chmod 644 "$KNOWN_HOSTS_PATH"
 chmod 600 "$WPENGINE_SSH_KEY_PRIVATE_PATH"
 chmod 644 "$WPENGINE_SSH_KEY_PUBLIC_PATH"
 
-# Change the working directory
-cd /github/workspace
+# Set the GIT_DIR environment variable
+export GIT_DIR=/github/workspace/.git
 
 git config core.sshCommand "ssh -i $WPENGINE_SSH_KEY_PRIVATE_PATH -o UserKnownHostsFile=$KNOWN_HOSTS_PATH"
 git remote add $WPENGINE_ENV git@$WPENGINE_HOST:$WPENGINE_ENV/$WPENGINE_ENVIRONMENT_NAME.git
